ce40e1002132c2c9ac453d50ea9d8a3f49472331
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2026-02-17 19:50:27 -0300

    feat: finish product service

--- products-service/src/app.module.ts
+++ products-service/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { PrismaModule } from './infra/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductsModule, PrismaModule,HealthModule],
   controllers: [],
   providers: [],
 })
