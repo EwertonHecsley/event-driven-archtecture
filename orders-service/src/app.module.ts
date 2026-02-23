25f04bf6bfa3e442e3f5538c03f41eec94e855ca
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2026-02-23 20:35:30 -0300

    feat: finalized order service

--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './infra/prisma/prisma.module';
+import { HealthModule } from './health/health.module';
+import { OrdersModule } from './orders/orders.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,HealthModule,OrdersModule
+  ],
   controllers: [],
   providers: [],
 })
--- orders-service/src/orders/orders.module.ts
+++ orders-service/src/orders/orders.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { OrdersController } from "./orders.controller";
 import { OrdersService } from "./orders.service";
 
-
 @Module({
   controllers: [OrdersController],
   providers: [OrdersService],
